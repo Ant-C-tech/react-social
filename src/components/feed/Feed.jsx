5d5aeada9066cc7b9f5ac52d734df1e5aa66e33f
achyvikov <anton.chyvikov@cengage.com>
2022-05-01 17:34:54 +0300

    feat: setup feed

---
+++ src/components/feed/Feed.jsx
@@ -0,0 +1,5 @@
+import './feed.css';
+
+export const Feed = () => {
+	return <div className='feed'>Feed</div>;
+};
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import './home.css';
 
 import { TopBar } from '../../components/topbar/TopBar';
 import { SideBar } from '../../components/sidebar/SideBar';
+import { Feed } from '../../components/feed/Feed';
 
 export const Home = () => {
 	return (<>
 		<TopBar />
 		<main className="container-flex">
-				<SideBar />
+			<SideBar />
+			<Feed />
 		</main>
 	</>)
 };
