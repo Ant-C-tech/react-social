16db49b33bf04ea0c219ce9f0e9def54d4e81a84
achyvikov <anton.chyvikov@cengage.com>
2022-05-01 16:52:29 +0300

    fix: topbar minor issues

--- src/components/common/customLink/CustomLink.jsx
+++ src/components/common/customLink/CustomLink.jsx
@@ -6,7 +6,7 @@ export const CustomLink = (props) => {
 	const { content, href, target, modification } = props;
 	return (
 		<a className={`link ${modification}`} href={href} target={target}>
-			{modification === 'icon' ? <Icon { ...props } /> : content}
+			{modification === 'icon' ? <Icon {...props} /> : content}
 		</a>
 	);
 };
