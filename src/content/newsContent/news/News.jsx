ecd08dff90fbc1a1e21dea52d3518a93a17289fc
achyvikov <anton.chyvikov@cengage.com>
2022-05-15 17:13:13 +0300

    fix: implement looping of the mock news array

--- src/content/newsContent/news/News.jsx
+++ src/content/newsContent/news/News.jsx
@@ -2,15 +2,15 @@ import './news.css';
 
 import { Info } from '@material-ui/icons';
 
-export const News = () => {
+export const News = ({ title, image, text }) => {
 	return (
 		<article className="news">
 			<header>
-				<Info className="news-title-icon"/>
-				<h2 className="news-title"> My first news</h2>
+				<Info className="news-title-icon" />
+				<h1 className="news-title">{title}</h1>
 			</header>
-			<img className="news-image" src="https://picsum.photos/500/800" alt="news that I want to talk to you about" />
-			<p className="news-text">"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+			<img className="news-image" src={image} alt={title} />
+			<p className="news-text">{text}</p>
 			<footer></footer>
 		</article>
 	)
