cb04307c5b6808b14af6fb514c5cda825bd0f791
achyvikov <anton.chyvikov@cengage.com>
2022-07-02 18:01:22 +0300

    fix: implement skeleton loader

--- src/pages/newsPage/newsFeed/NewsFeed.jsx
+++ src/pages/newsPage/newsFeed/NewsFeed.jsx
@@ -2,12 +2,13 @@ import './newsFeed.css';
 
 import { useEffect } from 'react'
 import uuid from 'react-uuid'
+import { SkeletonTheme } from 'react-loading-skeleton'
 
 import { useScrollTo } from '../hooks/useScrollTo';
 
 import { News } from './news/News';
 import { Message } from '../../../components/common/message/Message';
-import { Loader } from '../../../components/common/loader/Loader';
+import { NewsSkeleton } from './newsSkeleton/NewsSkeleton';
 
 export const NewsFeed = ({ newsSet, lastNewsRef, focusNewsIndex, loading }) => {
 	const [targetScrollRef, scrollToRef] = useScrollTo()
@@ -18,30 +19,34 @@ export const NewsFeed = ({ newsSet, lastNewsRef, focusNewsIndex, loading }) => {
 
 	return (
 		<section className='news-feed' >
-			{loading ? <Loader /> : newsSet.length > 0 ? <ul className='news-list' >
-				{newsSet.map((news, index) => {
-					return <li
-						key={uuid()}
-						className='news-list-item'
-						ref={index === newsSet.length - 1 ? lastNewsRef : index === focusNewsIndex - 2 ? targetScrollRef : null}>
-						<News
-							categories={news.category}
-							countries={news.country}
-							title={news.title}
-							image={news.image_url}
-							text={news.description}
-							pubDate={news.pubDate}
-							creators={news.creator}
-							link={news.link}
-							video={news.video_url}
-							language={ news.language}
-						/>
-					</li>
-				})}
-			</ul> : <Message type={'info'} title={'Nothing was found according to your request.'}>
-				<p>Try to change your search parameters.</p>
-				<p>Happy news!</p>
-			</Message>
+			{loading ?
+				<SkeletonTheme baseColor="#dce2e4" highlightColor="#96a9af">
+					<NewsSkeleton skeletons={3} />
+				</SkeletonTheme>
+				: newsSet.length > 0 ? <ul className='news-list' >
+					{newsSet.map((news, index) => {
+						return <li
+							key={uuid()}
+							className='news-list-item'
+							ref={index === newsSet.length - 1 ? lastNewsRef : index === focusNewsIndex - 2 ? targetScrollRef : null}>
+							<News
+								categories={news.category}
+								countries={news.country}
+								title={news.title}
+								image={news.image_url}
+								text={news.description}
+								pubDate={news.pubDate}
+								creators={news.creator}
+								link={news.link}
+								video={news.video_url}
+								language={news.language}
+							/>
+						</li>
+					})}
+				</ul> : <Message type={'info'} title={'Nothing was found according to your request.'}>
+					<p>Try to change your search parameters.</p>
+					<p>Happy news!</p>
+				</Message>
 			}
 		</section>
 	);
---
+++ src/pages/newsPage/newsFeed/newsSkeleton/NewsSkeleton.jsx
@@ -0,0 +1,62 @@
+import 'react-loading-skeleton/dist/skeleton.css'
+
+import './newsSkeleton.css';
+
+import Skeleton from 'react-loading-skeleton'
+
+export const NewsSkeleton = ({ skeletons }) => {
+	return (
+		Array(skeletons).fill(0).map((_, index) =>
+			<div className="news-skeleton" key={index}>
+				<div className="news-skeleton-header">
+					<div className="news-skeleton-category">
+						<Skeleton />
+					</div>
+					<div className="news-skeleton-country">
+						<Skeleton />
+					</div>
+				</div>
+
+				<div className="news-skeleton-title">
+					<div className="news-skeleton-title-first-line" >
+						<div className="news-skeleton-title-icon">
+							<Skeleton />
+						</div>
+						<div className="news-skeleton-title-icon-text">
+							<Skeleton />
+						</div>
+					</div>
+					<div className="news-skeleton-title-text">
+						<Skeleton />
+					</div>
+					<div className="news-skeleton-title-text">
+						<Skeleton />
+					</div>
+				</div>
+
+				<div className="news-skeleton-img">
+					<Skeleton height={150} />
+				</div>
+
+				<Skeleton count={3} />
+				<div className="news-skeleton-read-more">
+					<div className="news-skeleton-read-more-icon">
+						<Skeleton />
+					</div>
+					<div className="news-skeleton-read-more-text">
+						<Skeleton />
+					</div>
+				</div>
+
+				<div className="news-skeleton-footer">
+					<div className="news-skeleton-data">
+						<Skeleton />
+					</div>
+					<div className="news-skeleton-author">
+						<Skeleton />
+					</div>
+				</div>
+			</div>
+		)
+	)
+};
